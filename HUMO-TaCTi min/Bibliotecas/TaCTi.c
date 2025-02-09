5da5e1d75424a68e95bdd8303dbb86f74fd1a2e1
Maximiano Mascasin Muñoz <maxi.mascasin@gmail.com>
2025-02-08 21:20:59 -0300

    pain

--- HUMO-TaCTi min/Bibliotecas/TaCTi.c
+++ HUMO-TaCTi min/Bibliotecas/TaCTi.c
@@ -1,6 +1,81 @@
 #include "TaCTi.h"
 
+void menu()
+{
+    char opcion;
+    int partidaJugada = 0;
+    tLista lista;
+    t_cola cola;
+    Config config;
 
+    // Crear lista y cola
+    crearLista(&lista);
+    crearCola(&cola);
+
+    // Leer configuración desde el archivo
+    if (!leerConfig("config.txt", &config))
+    {
+        printf("Error al cargar la configuración. Saliendo del programa...\n");
+        return;
+    }
+
+    srand(time(NULL));
+    system("cls");
+
+    printf("\033[5;1;36m|------- Bienvenido a TaCTi -------|\n");
+    printf("\033[0m");
+
+    do
+    {
+        printf("[A] Jugar\n");
+        printf("[B] Ver ranking equipo\n");
+        printf("[C] Salir\n");
+        printf("\n");
+        printf("Seleccione una opcion: ");
+        scanf("%c", &opcion);
+        getchar();
+
+        switch (opcion)
+        {
+        case 'A':
+        case 'a':
+            printf("\nCargando jugadores y coordenadas...\n");
+
+            // Cargar jugadores y coordenadas
+            cargarJugadores(&lista);
+
+            if(!partidaJugada){
+                cargarCoordenadas(&cola);
+            }
+
+            // Iniciar el juego con la cantidad de partidas de la configuración
+            iniciarJuego(&lista, &cola, config.cantidadPartidas);
+
+            partidaJugada = 1;
+
+            // Vaciar la lista después del juego para evitar datos colgados
+            vaciarLista(&lista);
+            break;
+
+        case 'B':
+        case 'b':
+            printf("\nNope.\n");
+            break;
+
+        case 'C':
+        case 'c':
+            printf("\nSaliendo del programa. Hasta luego!\n");
+            break;
+
+        default:
+            printf("\nOpción no válida. Intente nuevamente.\n");
+            printf("\nPresione cualquier tecla para continuar...\n");
+            getchar(); // Esperar una tecla para continuar
+            break;
+        }
+    }
+    while (opcion != 'C' && opcion != 'c');
+}
 
 void iniciarJuego(tLista *pl, t_cola *cCoordenadas, int maxPartidas)
 {
@@ -53,13 +128,13 @@ int iniciarPartida(tJugador *jugadorActual, t_cola *cCoordenadas)
             do
             {
                 do{
-                    printf("Seleccione una coordenada \033[0;35mX\033[0m del tablero: ");
+                    printf("Seleccione una coordenada \033[0;35mY\033[0m del tablero: ");
                     scanf("%d", &coordenadaX);
                     getchar();
                 }while(coordenadaX!=0 && coordenadaX!=1 && coordenadaX !=2);
 
                 do{
-                    printf("Seleccione una coordenada \033[1;34mY\033[0m del tablero: ");
+                    printf("Seleccione una coordenada \033[1;34mX\033[0m del tablero: ");
                     scanf("%d", &coordenadaY);
                     getchar();
                 }while(coordenadaY!=0 && coordenadaY!=1 && coordenadaY !=2);
